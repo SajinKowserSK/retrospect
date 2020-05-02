e9477f4e6735de94be127f5e4788f863c88f749a
shafinsiddique <siddiqueshafin20@gmail.com>
2020-05-02 13:36:26 -0400

    fixed api design

--- api/mentorCollectionHelper.js
+++ api/mentorCollectionHelper.js
@@ -4,29 +4,38 @@ const db_name = "billiondollar";
 const collection_name = "mentors";
 const querystring = require('querystring');
 
-exports.getAllMentors = function (req, res) {
-  MongoClient.connect(db_url, function (err, db) {
+function getMentorsWithKeywords(database, req, res) {
+  keywords = req.query['keywords'].split(',');
+  database.collection(collection_name).find({ "keywords": { "$in": keywords } }).toArray(function (err, result) {
     if (err) throw err;
-    var dbo = db.db(db_name);
-    dbo.collection(collection_name).find({}).toArray(function (err, result) {
-      if (err) throw err;
-      res.send(JSON.stringify(result));
-      db.close();
-    });
-  });
-}
-
-exports.findMentorsWithKeywords = function (req, res) {
-  var keywords_from_query = req.query['keywords'].split(',');
-  MongoClient.connect(db_url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db(db_name);
-    dbo.collection(collection_name).find({ "keywords": { "$in":keywords_from_query} }).toArray(function (err, result) {
-      if (err) throw err;
-      res.send(JSON.stringify(result));
-      db.close();
-    });
+    res.send(JSON.stringify(result));
   });
 
 }
+
+function getAllMentors(database, req, res) {
+  database.collection(collection_name).find({}).toArray(function (err, result) {
+    if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+}
+
+exports.getMentors = function (req, res) {
+  MongoClient.connect(db_url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(db_name);
+    if ('keywords' in req.query) {
+      getMentorsWithKeywords(dbo, req, res);
+    }
+
+    else {
+      getAllMentors(dbo, req, res);
+      
+    }
+    db.close();
+
+
+  });
+}
+
 
--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
     var mentorsCollection = require("./mentorCollectionHelper");
-    app.route('/mentors').get(mentorsCollection.getAllMentors);
-    app.route('/mentors/keyword').get(mentorsCollection.findMentorsWithKeywords);
+    app.route('/mentors').get(mentorsCollection.getMentors);
+    // app.route('/mentors/keyword').get(mentorsCollection.findMentorsWithKeywords);
 }
