34a9d62facb894a5ba87815b11bd1eb3aa36b6a3
shafinsiddique <siddiqueshafin20@gmail.com>
2020-05-02 13:00:10 -0400

    full migeation to node js.

--- api/mentorCollectionHelper.js
+++ api/mentorCollectionHelper.js
@@ -1,18 +1,32 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://shafinsiddique:Ishafin98@cluster0-jrksj.mongodb.net/test?retryWrites=true&w=majority";
-var db_name = "billiondollar";
-var collection_name = "mentors";
+const MongoClient = require('mongodb').MongoClient;
+const db_url = "mongodb+srv://shafinsiddique:Ishafin98@cluster0-jrksj.mongodb.net/test?retryWrites=true&w=majority";
+const db_name = "billiondollar";
+const collection_name = "mentors";
+const querystring = require('querystring');
 
 exports.getAllMentors = function (req, res) {
-        MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db(db_name);
-        dbo.collection(collection_name).find({}).toArray(function(err, result) {
-          if (err) throw err;
-          res.send(JSON.stringify(result));
-          db.close();
-        });
-      });
+  MongoClient.connect(db_url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(db_name);
+    dbo.collection(collection_name).find({}).toArray(function (err, result) {
+      if (err) throw err;
+      res.send(JSON.stringify(result));
+      db.close();
+    });
+  });
+}
+
+exports.findMentorsWithKeywords = function (req, res) {
+  var keywords_from_query = req.query['keywords'].split(',');
+  MongoClient.connect(db_url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(db_name);
+    dbo.collection(collection_name).find({ "keywords": { "$in":keywords_from_query} }).toArray(function (err, result) {
+      if (err) throw err;
+      res.send(JSON.stringify(result));
+      db.close();
+    });
+  });
 
 }
 
--- api/routes.js
+++ api/routes.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
     var mentorsCollection = require("./mentorCollectionHelper");
     app.route('/mentors').get(mentorsCollection.getAllMentors);
+    app.route('/mentors/keyword').get(mentorsCollection.findMentorsWithKeywords);
 }
