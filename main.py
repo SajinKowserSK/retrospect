99fb6a083f30841751a8663296731e7337b18a80
shafinsiddique <siddiqueshafin20@gmail.com>
2020-05-01 23:47:12 -0400

    implemented search query.

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import requests
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
-api_base_url =
+api_base_url = "http://127.0.0.1:5001/"
 def home():
     if request.method == "GET":
         return render_template("index.html")
 
-    return render_template("mentors.html", mentors=requests.get(""))
+    return render_template("mentors.html",
+                           mentors=requests.get(api_base_url + "/mentor/?keywords="+request.form['keywords']))
 
