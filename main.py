970e3d921d441aff723f54e7fabdd9a1629391a5
Sajin Kowser <sajinkowser@gmail.com>
2020-05-03 16:27:20 -0400

    Added cards to mentor display

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
-api_base_url = "http://127.0.0.1:5001/"
+api_base_url = "http://localhost:3000/"
 
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -9,5 +9,5 @@ def home():
         return render_template("index.html")
     else:
         return render_template("mentors.html",
-                               mentors=requests.get(api_base_url + "mentor/?keywords="+request.form['keywords']).json())
+                               mentors=requests.get(api_base_url + "mentors/?keywords="+request.form['keywords']).json())
 
