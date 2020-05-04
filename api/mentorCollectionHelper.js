19dfe0a1f35eab5905eda7488be1d21b8b0cf984
shafinsiddique <siddiqueshafin20@gmail.com>
2020-05-04 01:00:01 -0400

    changed code structure to avoid duplication.

--- api/mentorCollectionHelper.js
+++ api/mentorCollectionHelper.js
@@ -4,49 +4,28 @@ const db_name = "billiondollar";
 const collection_name = "mentors";
 const querystring = require('querystring');
 
-function getMentorsWithKeywords(database, req, res) {
-  keywords = req.query['keywords'].split(',').map(keyword_string => keyword_string.trim());
-  database.collection(collection_name).find({ "keywords": { "$all": keywords } }).toArray(function (err, result) {
+function searchMentorsCollectionAndSendResponse(database, response, query_dict) {
+  database.collection(collection_name).find(query_dict).toArray(function (err, result) {
     if (err) throw err;
-    res.send(JSON.stringify(result));
-  });
-
-}
-
-function getMentorWithEmail(database, req, res) {
-  database.collection(collection_name).find({"email":req.query['email']}).toArray(function (err, result) {
-    if (err) throw err;
-    res.send(JSON.stringify(result));
-  }) 
-    
-}
-
-
-function getAllMentors(database, req, res) {
-  database.collection(collection_name).find({}).toArray(function (err, result) {
-    if (err) throw err;
-    res.send(JSON.stringify(result));
+    response.send(JSON.stringify(result));
   });
 }
-
 
 exports.getMentors = function (req, res) {
   MongoClient.connect(db_url, function (err, db) {
     if (err) throw err;
     var dbo = db.db(db_name);
+    var query_dict =  {}
     if ('keywords' in req.query) {
-      getMentorsWithKeywords(dbo, req, res);
+      keywords = req.query['keywords'].split(',').map(keyword_string => keyword_string.trim());
+      query_dict = { "keywords": { "$all": keywords }};
     }
 
     else if ('email' in req.query){ 
-      getMentorWithEmail(dbo, req, res);
+      query_dict = {"email":req.query['email']}
     }
 
-    else {
-      getAllMentors(dbo, req, res);
-    }
+    searchMentorsCollectionAndSendResponse(dbo, res, query_dict);
     db.close();
   });
 }
-
-
