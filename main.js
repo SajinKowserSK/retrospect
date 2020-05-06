840e4de0bed28d44a5abee6fbb55899c17e1df76
shafinsiddique <siddiqueshafin20@gmail.com>
2020-05-06 02:52:21 -0400

    post request for home page in node.js, need to fix APi call.

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = express();
 var port = 5000;
 app.set('views','./views');
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/static'));
-routes(app); // create routes for the website.
+app.use(express.static(__dirname + '/static')); // set static folder to ./static.
+app.use(express.urlencoded()); // Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+routes(app); // create routes for the website, see routes.js file.
 app.listen(port);
 console.log('Web server running on port ' + port);
+
+
--- routes.js
+++ routes.js
@@ -1,6 +1,21 @@
 const path = require('path');
+const requests = require('request');
+const api_base_url = "http://localhost:3000/"
 module.exports = function(app) {
+    
     app.get('/', function(request, response) {
+        // handle what happens when we get a get request for the home page.
         response.render("index.ejs");
+    })
+
+    app.post('/', function(request, response) {
+        // handle what happens when we get a post request from the home page.
+        console.log("got post request");
+        var keywords = request.body.keywords;
+        request.get(api_base_url + "mentors/?keywords=" + keywords, {"json":true}, function(err, res, body) {
+            if (err) throw err;
+            console.log(body.explanation);
+        })
+        
     })
 }
