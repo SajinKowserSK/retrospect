8c87ee8982d557b374d3ff41c83ccddd116ecd8e
SajinKowserSK <sajinkowser@gmail.com>
2020-05-09 23:05:47 -0400

    fixed some redirecting url issues, but must debug keywords arg for profile.html

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 app = Flask(__name__)
 app.secret_key = "shafibullah"
@@ -36,7 +36,7 @@ def login():
         if response.status_code == 200:
             session['logged_in'] = True
 
-            return render_template("profile.html", status=session['logged_in'], name=responseDict['name'], bio=responseDict['bio'], pic=responseDict['image'], keywords=responseDict['keywords'], tags = tagColors)
+            return redirect(url_for('.profile', status=session['logged_in'], name=responseDict['name'], bio=responseDict['bio'], pic=responseDict['image'], keywords=responseDict['keywords'], tags = tagColors))
 
         elif response.status_code ==  404:
 
@@ -44,3 +44,12 @@ def login():
         else:
 
             return render_template("login.html", status=session['logged_in'], msgResponse = "Email found but incorrect password".upper())
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return render_template("index.html", status=session['logged_in'])
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", status=request.args['status'], name=request.args['name'], bio=request.args['bio'], pic=request.args['pic'], keywords=request.args['keywords'], tags=request.args['tags'])
