e1a241d868d2ce434cb1fe525534e00472a09582
SajinKowserSK <sajinkowser@gmail.com>
2020-05-10 22:16:25 -0400

    updated to do list

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # billiondollar
+
+# To Do: 
+
+# Shafin
+- Start designing web landing page (use undraw "online connect" pic)
+- fix log out feature 
+
+
+# Sajin
+- get started on the registration form
+- create design colors 
--- main.py
+++ main.py
@@ -10,8 +10,10 @@ api_base_url = "https://barebilliondollar.herokuapp.com/"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(email):
+    print(email)
     return requests.get(api_base_url + "mentors/?email="+email).json()[0]
 
 @app.route('/',methods=['GET','POST'])
