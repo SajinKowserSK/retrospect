9f86d850bf9ff433c48f0b9a905f8edc62426ffd
SajinKowserSK <sajinkowser@gmail.com>
2020-08-19 03:04:35 -0400

    some progress has been made..need to still position the messages correctly

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ def handle_send_message_event(data):
 def handle_join_room_event(data):
     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
     join_room(data['room'])
-    socketio.emit('join_room_announcement', data, room=data['room'])
+    #socketio.emit('join_room_announcement', data, room=data['room'])
 
 
 @socketio.on('leave_room')
 def handle_leave_room_event(data):
     app.logger.info("{} has left the room {}".format(data['username'], data['room']))
     leave_room(data['room'])
-    socketio.emit('leave_room_announcement', data, room=data['room'])
+    #socketio.emit('leave_room_announcement', data, room=data['room'])
 
 @login_manager.user_loader
 def load_user(email):
